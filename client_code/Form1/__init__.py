76359412328c4b42c3c5ddacc5f3bcae152f26a7
walternzd@gmail.com <walternzd@gmail.com>
2024-12-07 06:15:07 +0000

    feat(ui): add floating box notification popup Implemented a notification popup component to display real-time updates to the user

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -10,3 +10,7 @@ class Form1(Form1Template):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     # Any code you write here will run before the form opens.
+
+  def drop_down_stocks_change(self, **event_args):
+    """This method is called when an item is selected"""
+    alert("You selecred a stock " + self.drop_down_stocks.selected_value)
