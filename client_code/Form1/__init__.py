0f29ae21f5588a1609df590888caef5af8f45c29
Walter462 <74890413+Walter462@users.noreply.github.com>
2024-12-09 09:23:48 +0800

    docs(obsidian): add links

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -13,15 +13,16 @@ class Form1(Form1Template):
     # Any code you write here will run before the form opens.
     self.drop_down_stocks.items = anvil.server.call('getTickers')
 
-  def drop_down_stocks_change(self, **event_args):
-    """This method is called when an item is selected"""
-    #alert("You selecred a stock " + self.drop_down_stocks.selected_value)
+  # | NOTE: Dropdown list behaviour on show and select (call registered server functions)  | File: KnowlegeBase/IT/Development/DataDrivenApps/Anvil/Tutorials/StockPicker | ID: 1733643744 |
+  def drop_down_stocks_show(self, **event_args):
+    """This method is called when the DropDown is shown on the screen"""
     ticker = self.drop_down_stocks.selected_value
     self.stockTicker.text = ticker
     self.stockPrice.text = anvil.server.call('getPrice', ticker)
 
-  def drop_down_stocks_show(self, **event_args):
-    """This method is called when the DropDown is shown on the screen"""
+  def drop_down_stocks_change(self, **event_args):
+    """This method is called when an item is selected"""
+    #alert("You selecred a stock " + self.drop_down_stocks.selected_value)
     ticker = self.drop_down_stocks.selected_value
     self.stockTicker.text = ticker
     self.stockPrice.text = anvil.server.call('getPrice', ticker)
--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -2,7 +2,7 @@ import anvil.secrets
 import anvil.server
 import requests 
 
-
+# GitHub as MD link
 # | NOTE: Securely setting FinnHub API as constant  | File: KnowlegeBase/IT/Development/DataDrivenApps/Anvil/Tutorials/StockPicker | ID: 1733641711 |
 FINNHUB_KEY = anvil.secrets.get_secret("FinnHubAPI")
 # This is a server module. It runs on the Anvil server,
@@ -18,11 +18,13 @@ FINNHUB_KEY = anvil.secrets.get_secret("FinnHubAPI")
 #   return 42
 #
 
+# | NOTE: Hardcoded stock names for dropdown menu | File: KnowlegeBase/IT/Development/DataDrivenApps/Anvil/Tutorials/StockPicker | ID: 1733644314 |
 @anvil.server.callable
 def getTickers():
   tickers =['AAPL', 'TSLA', 'GME', 'AMC']
   return(tickers)
 
+# | NOTE: External API call | File: KnowlegeBase/IT/Development/DataDrivenApps/Anvil/Tutorials/StockPicker | ID: 1733642894 |
 @anvil.server.callable
 def getPrice(ticker):
   r = requests.get(f'https://finnhub.io/api/v1/quote?symbol={ticker}&token={FINNHUB_KEY}')
